8f0f83b5ca0f3e1718aca198d0bcc010a4cc4e46
YanVL <yanvitor.gti@gmail.com>
2021-07-14 02:57:29 -0300

    Tratando erros com excecao personalizada

--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -19,15 +19,43 @@
             return pecas[linha, coluna];
         }
 
-        public void colocarPeca(Peca p, Posicao pos) //Coloca a peca informada na posicao informada
+        public Peca peca(Posicao pos)
         {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        public void colocarPeca(Peca p, Posicao pos) //Coloca a peca informada na posicao informada 
+        {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
 
+        public bool existePeca(Posicao pos) //Teste se existe peca na posicao e se a posicao e valida
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
 
+        public bool posicaoValida(Posicao pos) //Testa se a posição e valida no tabuleiro
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
 
-
+        public void validarPosicao(Posicao pos) //Complemento da posicaoValida, lancando uma excecao personalizada
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida");
+            }
+        }
 
 
     }
